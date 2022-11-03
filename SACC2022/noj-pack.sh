7b5abe476cde7e8104e9c6aeffdaef59023761ef
0xfaner <0xfaner@gmail.com>
2022-11-03 21:35:45 +0800

    add noj pack script

---
+++ SACC2022/noj-pack.sh
@@ -0,0 +1,35 @@
+scriptRoot=$(dirname "$0" | sed 's?\\?\/?g')
+
+NOJRoot=$scriptRoot/NOJ
+
+problemRoot=$scriptRoot/NOJ/problems
+solutionRoot=$scriptRoot/NOJ/solution
+dataRoot=$scriptRoot/NOJ/data
+
+cleanDir() {
+    if [ -d $* ]; then
+        rm -rf $*
+        if [[ $? -ne 0 ]]; then
+            echo failed
+            exit
+        fi
+    fi
+    
+    mkdir $*
+    if [[ $? -ne 0 ]]; then
+        echo failed
+        exit
+    fi
+}
+
+cleanDir ${NOJRoot}
+cleanDir ${problemRoot}
+cleanDir ${solutionRoot}
+cleanDir ${dataRoot}
+
+array=(A B C D E F G H)
+for i in "${array[@]}"; do
+    cat $scriptRoot/$i/problem.md | sed -r -e 's?\$?$$$?g' | sed -r -e 's?\\?\\\\?g' > ${problemRoot}/$i.md
+    cat $scriptRoot/$i/solution.md > ${solutionRoot}/$i.md
+    cp -r $scriptRoot/$i/data ${dataRoot}/$i/
+done
